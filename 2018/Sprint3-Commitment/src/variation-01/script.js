6342c3646a28e9b13644ab9ca8b51afb0af7d677
Max Vith <max.vith@web-arts.com>
2018-07-02 08:48:36 +0200

    push test

--- 2018/Sprint3-Commitment/src/variation-01/script.js
+++ 2018/Sprint3-Commitment/src/variation-01/script.js
@@ -58,68 +58,119 @@
             });
         }
 
+        function showLayer() {
+
+            manageClass($waSurvey, 'hide');
+            manageClass($waLayerBG, 'show');
+            manageClass($waLayer, 'show');
+        }
+
+        function hideLayer() {
+
+            manageClass($waLayerBG, 'show', true);
+            manageClass($waLayer, 'show', true);
+        }
+
         smoBoxContainer[0].insertAdjacentHTML('afterend', 
             '<div id="wa-survey" class="wa-small wa-first">' + 
                 '<div id="wa-pulse" class="wa-animated">1</div>' +
                 '<div id="wa-badge" class="wa-animated"></div>' +
+                '<div id="wa-wrapper">' + 
+                    '<h1>Hier</h1>' +
+                    '<button type="button">klick</button>' +
+                '</div>' +
+            '</div>' +
+            '<div id="wa-layer-bg"></div>' +
+            '<div id="wa-layer">' + 
+                '<div id="wa-layer-close">X</div>' +
             '</div>'
         );
 
-        var $waSurvey   = WATO.qs('#wa-survey'),
-            $waPulse    = WATO.qs('#wa-pulse'),
-            $waBadge    = WATO.qs('#wa-badge');
+        var $waSurvey         = WATO.qs('#wa-survey'),
+            $waSurveyPulse    = WATO.qs('#wa-pulse', $waSurvey),
+            $waSurveyBadge    = WATO.qs('#wa-badge', $waSurvey),
+            $waSurveyButton   = WATO.qs('#wa-wrapper button', $waSurvey),
+            $waLayerBG        = WATO.qs('#wa-layer-bg'),
+            $waLayer          = WATO.qs('#wa-layer'),
+            $waLayerClose      = WATO.qs('#wa-layer-close');
 
         // set pulse animation after 1500ms
         window.setTimeout(function(){
 
-            manageClass($waPulse, 'pulse');
+            manageClass($waSurveyPulse, 'pulse');
         }, 1500);
 
+        /**
+         * SURVERY EVENTS
+         */
         // survery mouseover
-        addListener($waSurvey, 'mouseover', function(){
+        // addListener($waSurvey, 'mouseover', function(){
 
-            // remove class wa-first, hide wa-pulse element
-            manageClass($waSurvey, 'first', true);
+        //     // remove class wa-first, hide wa-pulse element
+        //     manageClass($waSurvey, 'first', true);
 
-            // if(this.classList.contains('wa-small')){
-            if(classContains(this, 'small')){
+        //     // if(this.classList.contains('wa-small')){
+        //     if(classContains(this, 'small')){
 
-                manageClass($waBadge, 'fade-out', true);
-                manageClass($waBadge, 'fade-in');
-            }
-        });
+        //         manageClass($waSurveyBadge, 'fade-out', true);
+        //         manageClass($waSurveyBadge, 'fade-in');
+        //     }
+        // });
 
-        // survery mouseout
-        addListener($waSurvey, 'mouseout', function(){
+        // // survery mouseout
+        // addListener($waSurvey, 'mouseout', function(){
 
-            // if(this.classList.contains('wa-small')){
-            if(classContains(this, 'small')){
+        //     // if(this.classList.contains('wa-small')){
+        //     if(classContains(this, 'small')){
 
-                manageClass($waBadge, 'fade-in', true);
-                manageClass($waBadge, 'fade-out');
-            // } else if(this.classList.contains('wa-large')){
-            } else if(classContains(this, 'large')){
+        //         manageClass($waSurveyBadge, 'fade-in', true);
+        //         manageClass($waSurveyBadge, 'fade-out');
+        //     // } else if(this.classList.contains('wa-large')){
+        //     } else if(classContains(this, 'large')){
 
-                animate("-105", "-519", function() {
+        //         animate("-105", "-519", function() {
     
-                    manageClass($waSurvey, 'large', true);
-                    manageClass($waSurvey, 'small');
-                    manageClass($waBadge, 'fade-out');
-                });
-            }
-        });
+        //             manageClass($waSurvey, 'large', true);
+        //             manageClass($waSurvey, 'small');
+        //             manageClass($waSurveyBadge, 'fade-out');
+        //         });
+        //     }
+        // });
 
-        addListener($waSurvey, 'click', function(){
+        // addListener($waSurvey, 'click', function(){
 
-            manageClass($waSurvey, 'animate');
+        //     manageClass($waSurvey, 'animate');
 
-            animate("57", "0", function() {
+        //     animate("57", "0", function() {
     
-                manageClass($waSurvey, 'small', true);
-                manageClass($waSurvey, 'animate', true);
-                manageClass($waSurvey, 'large');
-            });
+        //         manageClass($waSurvey, 'small', true);
+        //         manageClass($waSurvey, 'animate', true);
+        //         manageClass($waSurvey, 'large');
+        //     });
+        // });
+
+        /**
+         * LAYER EVENTS
+         */
+        // window.onscroll = function(){
+        //     var top  = window.pageYOffset || document.documentElement.scrollTop
+        // };
+
+        addListener($waSurveyButton, 'click', function(){
+
+            showLayer();
         });
+
+        addListener($waLayerBG, 'click', function(){
+
+            hideLayer();
+        });
+
+        addListener($waLayerClose, 'click', function(){
+
+            hideLayer();
+        });
+
     });
 
 })(new window.WATO());
