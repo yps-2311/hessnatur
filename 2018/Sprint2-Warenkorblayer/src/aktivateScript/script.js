6a40fc2510b2ee6161f953c595901080dd390d15
Max Vith <max.vith@web-arts.com>
2018-07-17 08:09:36 +0200

    100% Ausspielung LIVE

--- 2018/Sprint2-Warenkorblayer/src/aktivateScript/script.js
+++ 2018/Sprint2-Warenkorblayer/src/aktivateScript/script.js
@@ -10,45 +10,46 @@
 
     try {
 
-        if(document.cookie.indexOf("iridion_debug=true") !== -1){
+        // if(document.cookie.indexOf("iridion_debug=true") !== -1){
+        if(new RegExp(/.*hessnatur.com\/de\/.*\/p\/.*/).test(document.URL)){    
 
-            if(new RegExp(/.*hessnatur.com\/de\/.*\/p\/.*/).test(document.URL)){    
+            var request = XMLHttpRequest.prototype.open;
 
-                var request = XMLHttpRequest.prototype.open;
+            XMLHttpRequest.prototype.open = function(method, uri, async, user, pass) {this.addEventListener("loadend", function() {
 
-                XMLHttpRequest.prototype.open = function(method, uri, async, user, pass) {this.addEventListener("loadend", function() {
-                    if (this.readyState === 4) {
-                        if (uri.indexOf('https://www.hessnatur.com/de/cart/add') !== -1) {
-                            // console.log("activate push");
+                if (this.readyState === 4) {
 
-                            // try {
-                            //     // Minicart schnell ausbelden damit man es nicht beim AddToCart sieht
-                            //     var pMiniCart = document.getElementById("miniCartDropdown");
+                    if(uri.indexOf('https://www.hessnatur.com/de/cart/add') !== -1) {
+                        // console.log("activate push");
 
-                            //     if(pMiniCart){
-                            //         pMiniCart.style.display = "none";
+                        // try {
+                        //     // Minicart schnell ausbelden damit man es nicht beim AddToCart sieht
+                        //     var pMiniCart = document.getElementById("miniCartDropdown");
 
-                            //         setTimeout(function(){
-                            //             // Minicart wieder einblendbar machen
-                            //             pMiniCart.style.display = "inherit";
-                            //         }, 3000);
-                            //     }
-                            // } catch (error) {
-                            //     // console.log(error);
-                            // }
+                        //     if(pMiniCart){
+                        //         pMiniCart.style.display = "none";
 
-                            // In den Test
-                            // window.iridion.push(["run", "1528721919728"]);
+                        //         setTimeout(function(){
+                        //             // Minicart wieder einblendbar machen
+                        //             pMiniCart.style.display = "inherit";
+                        //         }, 3000);
+                        //     }
+                        // } catch (error) {
+                        //     // console.log(error);
+                        // }
 
-                            // MV, 16.07.2018, 100% Ausspielung
-                            window.iridion.push(["run", "1531735624736"]);
-                        }
+                        // In den Test
+                        // window.iridion.push(["run", "1528721919728"]);
+
+                        // MV, 16.07.2018, 100% Ausspielung
+                        window.iridion.push(["run", "1531735624736"]);
                     }
-                }, false);
+                }
+            }, false);
 
-                    request.call(this, method, uri, async, user, pass);
-                };
-            }
+                request.call(this, method, uri, async, user, pass);
+            };
         }
+        // }
     } catch(error){}
 })();
