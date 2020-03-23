c7060be9595f112dd8017a58de9b3f01b7cd4960
Nguyet Dang <yen-nguyet.dang@web-arts.com>
2020-03-23 18:55:54 +0100

    QS & Go live Sprint 15

--- 2020/06-Sprint-15-SEO-Info/variation-01/script.js
+++ 2020/06-Sprint-15-SEO-Info/variation-01/script.js
@@ -16,55 +16,68 @@
         window.setTimeout(function () {
 
             document.documentElement.classList.remove('kk_showSEO');
-        }, 250);
+        }, 550);
         WATO.qs('#kk_seoClose').classList.remove('kk_show');
     }
 
+
+    // SEO text wrapper
+    // only do stuff if element is given
     WATO.elem('.footerSmoBoxWrapper .rteContainer', function (seoText) {
-        console.log('has seo text');
-        WATO.elem('.toggleFilterButton', function (filterToggle) {
-            if (filterToggle) {
-                filterToggle[0].insertAdjacentHTML('beforebegin',
-                    '<div id="kk_toggleSEO">Mehr erfahren</div>'
-                );
+        if (seoText) {
+            WATO.elem('.toggleFilterButton', function (filterToggle) {
+                if (filterToggle) {
+                    // Add toggle
+                    filterToggle[0].insertAdjacentHTML('beforebegin',
+                        '<div id="kk_toggleSEO">Mehr erfahren</div>'
+                    );
 
-                filterToggle[0].previousElementSibling.addEventListener('click', function () {
-                    WATO.qs('#offCanvasMiniCartToggle').click();
-                    document.documentElement.classList.add('kk_showSEO');
+                    // Click listener for toggle
+                    filterToggle[0].previousElementSibling.addEventListener('click', function () {
+                        // Click on cart symbol to trigger layer
+                        WATO.qs('#offCanvasMiniCartToggle').click();
 
-                    // window.setTimeout(function () {
+                        // Add class for styling purpose
+                        document.documentElement.classList.add('kk_showSEO');
 
-                    WATO.qs('#kk_seoClose').classList.add('kk_show');
-                    // }, 250);
+                        // Close btn needs different timing for showing/hiding thus gets a separate class
+                        WATO.qs('#kk_seoClose').classList.add('kk_show');
 
-                    WATO.elem('.js-off-canvas-overlay.is-visible', function (overlay) {
-                        if (overlay) {
-                            overlay[0].addEventListener('click', hideSEO);
-                        }
+                        // Add click listener to overlay
+                        // On click remove the class 
+                        WATO.elem('.js-off-canvas-overlay.is-visible', function (overlay) {
+                            if (overlay) {
+                                overlay[0].addEventListener('click', hideSEO);
+                            }
+                        });
+
                     });
 
-                });
-                document.body.insertAdjacentHTML('afterbegin',
-                    '<div id="kk_seoClose">×</div>'
-                );
-                WATO.qs('#kk_seoClose').addEventListener('click', function () {
-                    WATO.qs('#offCanvasMiniCartWrapper .close-button').click();
-                    hideSEO();
-                });
+                    // Add own close btn
+                    document.body.insertAdjacentHTML('afterbegin',
+                        '<div id="kk_seoClose">×</div>'
+                    );
+                    // Add click listener to close btn
+                    WATO.qs('#kk_seoClose').addEventListener('click', function () {
+                        // Click on control close btn to close layer
+                        WATO.qs('#offCanvasMiniCartWrapper .close-button').click();
+                        hideSEO();
+                    });
 
-            }
-        });
+                }
+            });
 
-
-        WATO.elem('#offCanvasRight', function (canvasContent) {
-            if (canvasContent) {
-                canvasContent[0].insertAdjacentHTML('beforeend',
-                    // '<div class="off-canvas position-right is-transition-push" id="kk_offCanvasRight">' +
-                    '<div id="kk_offCanvasSEO" class="offCanvasTarget">' + seoText[0].outerHTML + '</div>'
-                    // '</div>'
-                );
-            }
-        });
+            // Add SEO text to exisiting off canvas element (cart layer)
+            WATO.elem('#offCanvasRight', function (canvasContent) {
+                if (canvasContent) {
+                    canvasContent[0].insertAdjacentHTML('beforeend',
+                        // '<div class="off-canvas position-right is-transition-push" id="kk_offCanvasRight">' +
+                        '<div id="kk_offCanvasSEO" class="offCanvasTarget">' + seoText[0].outerHTML + '</div>'
+                        // '</div>'
+                    );
+                }
+            });
+        }
     });
 
 
