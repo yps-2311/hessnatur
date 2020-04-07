863845c1c173c6779e56f3a3fccd14ccddf27f1b
Nguyet Dang <yen-nguyet.dang@web-arts.com>
2020-04-07 15:58:32 +0200

    Started AB08 as 100%

--- 2020/03-Sprint-08-Warenkorb-Ersparnisse/100%-Ausspielung/script.js
+++ 2020/03-Sprint-08-Warenkorb-Ersparnisse/100%-Ausspielung/script.js
@@ -10,56 +10,56 @@
  * Save Category of product added to cart on PDP 
  * Hide early + activate Sprint 8 on cart 
  */
-(function(WATO) {
+(function (WATO) {
     "use strict";
 
-    if(document.URL.indexOf('/p/') !== -1) {
+    if (document.URL.indexOf('/p/') !== -1) {
         // Productpage
 
         // console.log('PDS');
 
         var kk_pi_cat = false,
-        getCat = function(so_id){
-            var ret = '';
-            switch(so_id) {
-                case "SO-001":
-                case "SO-015":
-                case "SO-018":
-                case "SO-020":
-                case "SO-021":
-                case "SO-030":
-                case "SO-040":
+            getCat = function (so_id) {
+                var ret = '';
+                switch (so_id) {
+                    case "SO-001":
+                    case "SO-015":
+                    case "SO-018":
+                    case "SO-020":
+                    case "SO-021":
+                    case "SO-030":
+                    case "SO-040":
                         ret = 'damen';
-                    break;
+                        break;
 
-                case "SO-002":
-                case "SO-008":
-                case "SO-016":
-                case "SO-031":
-                case "SO-041":
+                    case "SO-002":
+                    case "SO-008":
+                    case "SO-016":
+                    case "SO-031":
+                    case "SO-041":
                         ret = 'herren';
-                    break;
+                        break;
 
-                case "SO-004":
-                case "SO-032":
-                case "SO-042":
+                    case "SO-004":
+                    case "SO-032":
+                    case "SO-042":
                         ret = 'kids';
-                    break;
+                        break;
 
-                case "SO-005":
+                    case "SO-005":
                         ret = 'baby';
-                    break;
+                        break;
 
-                case "SO-007":
+                    case "SO-007":
                         ret = 'home';
-                    break;
-            }
-            return ret;
-        };
+                        break;
+                }
+                return ret;
+            };
 
 
 
-        WATO.elem(function(){
+        WATO.elem(function () {
             try {
                 kk_pi_cat = window.pi.category_id || window.basketTrackingObject.category_id;
 
@@ -67,68 +67,70 @@
 
                 return !!kk_pi_cat;
             }
-            catch(e) {}
+            catch (e) { }
             return false;
-        }, function(pi_found){
-            if(pi_found) {
+        }, function (pi_found) {
+            if (pi_found) {
                 // console.log('kk_pi_cat: '+kk_pi_cat);
                 // do nothing and its okay :D
             }
         });
 
-        WATO.ajax('cart/add', function(){
+        WATO.ajax('cart/add', function () {
             try {
                 var _ls = JSON.parse(localStorage.getItem('kk_cats') || '{}'),
-                // _group = window.emosSelectVariantEventPrototype.group.split('/')[0],
-                _group = getCat(kk_pi_cat),
-                _thisGroup = _ls[_group];
-                
+                    // _group = window.emosSelectVariantEventPrototype.group.split('/')[0],
+                    _group = getCat(kk_pi_cat),
+                    _thisGroup = _ls[_group];
+
                 // console.log(_group);
-                if(_group !== '') {
-                    if(!_thisGroup) {
+                if (_group !== '') {
+                    if (!_thisGroup) {
                         _thisGroup = 1;
                     }
                     else {
                         _thisGroup++;
                     }
-        
+
                     _ls[_group] = _thisGroup;
-        
+
                     localStorage.setItem('kk_cats', JSON.stringify(_ls));
                 }
             }
-            catch(e) {}
+            catch (e) { }
         });
     }
     else {
         // Cart
-        document.head.insertAdjacentHTML('beforeend','<style id="kk_hide">html{opacity: 0 !important}</style>');
+        document.head.insertAdjacentHTML('beforeend', '<style id="kk_hide">html{opacity: 0 !important}</style>');
         var kk_hide = WATO.qs('#kk_hide');
 
         // find bottom CTA so products a loaded
-		WATO.elem('.h-mediumOffset-bottom-inner .button.success', function(bottomCTA) {
-			if(bottomCTA) {
-				var _products = WATO.qsa('.listing__table--item'), 
-				_productCount = _products.length;
-	
-				// iterate all products and find savings
-				for(var i=0; i < _productCount; i++) {
-					var _thisProduct = _products[i],
-					_oldPrice = WATO.qs('.strikeValue', _thisProduct);
-	
-					if(_oldPrice) {
+        WATO.elem('.h-mediumOffset-bottom-inner .button.success', function (bottomCTA) {
+            if (bottomCTA) {
+                var _products = WATO.qsa('.listing__table--item'),
+                    _productCount = _products.length;
+
+                // iterate all products and find savings
+                for (var i = 0; i < _productCount; i++) {
+                    var _thisProduct = _products[i],
+                        _oldPrice = WATO.qs('.strikeValue', _thisProduct);
+
+                    if (_oldPrice) {
                         // active Test
-                        window.iridion.push(['run', '939393152827']);
-                        
+                        // window.iridion.push(['run', '939393152827']);
+                        // 100% Ausspielung
+                        window.iridion.push(['run', '949584067611']);
+
                         break;
-					}
+                    }
                 }
-                
+
                 kk_hide.innerHTML = '';
-			}
+            }
         });
-        
-        setTimeout(function(){
+
+        setTimeout(function () {
             kk_hide.innerHTML = '';
         }, 5000);
     }
