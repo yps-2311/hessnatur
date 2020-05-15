ca87051c224f893d616ba6b7b9a028a26d42204b
Denis Leno <denis.leno@web-arts.com>
2020-05-15 15:38:43 +0200

    fixed

--- 2020/07-Personalisierung-01-Unterscheidung-Interessent-u-Neukunde/src/variation-01/script.js
+++ 2020/07-Personalisierung-01-Unterscheidung-Interessent-u-Neukunde/src/variation-01/script.js
@@ -77,6 +77,41 @@
         }
     });
 
+    WATO.elem('.productInfoTop > div > .column.medium-10', function(productInfo){
+        if(productInfo){
+            productInfo[0].insertAdjacentHTML('afterbegin', 
+                '<div class="kk_quali">'+
+                    '<span>Natürliche Qualität für Ihr wohlbefinden</span>'+
+                    '<div class="kk_natur">'+
+                        'Höchste Qualität durch<br>reine BIO-Naturfasern'+
+                    '</div>'+
+                    '<div class="kk_stuecke">'+
+                        'Kleindungsstücke ohne<br>Nachteile für Menschen & Natur'+
+                    '</div>'+
+                    '<div class="kk_tropfen">'+
+                        'Tragekomfort ohne<br>Schadstoffbelastung für die Haut'+
+                    '</div>'+
+                '</div>'
+            );
+
+            var siegel = WATO.qs(".certificateWrapper", productInfo[0]).parentNode;
+            if(siegel){
+                siegel.classList.add('kk_siegel');
+                productInfo[1].insertAdjacentElement('beforeend', siegel);
+            }
+            
+        }
+    });
+
+    WATO.elem('.pds-productImage__mzKeyVisualWrapper', function(productImage){
+        if(productImage){
+            console.log('productImage: ', productImage);
+            productImage[1].insertAdjacentHTML('beforeend', 
+                '<div><b>Aktuell sehr gefragt</b> Dieser Artikel verkauft sich schnell</div>'
+            );
+
+        }
+    });
     
     
 
---
+++ 2020/08-Factfinder-Personalisierung/prototyp.js
@@ -0,0 +1,30 @@
+
+
+// http://ffbackoffice.hessnatur.com/FF73-Query/Search.ff?username=hessnatur&password=d6648543b7ec3c456e38f93d7187a9d8&channel=LIVE-DE-Online-de&format=json&query=matratze
+
+
+
+// 195.244.100.161 [07/May/2020:11:25:01 +0200] 43F70A4EF6C1CE8EAC794255F427B10F "GET 
+// /FF73-Indexer/Tracking.ff?event=click&username=hessnatur&password=d6648543b7ec3c456e38f93d7187a9d8&channel=LIVE-DE-Online-de&sid=s39187136391984&id=47936&masterId=47936&title=Frottee-Handtuch%20aus%20reiner%20Bio-Baumwolle&query=*&pos=21&origPos=14&page=1&origPageSize=48 HTTP/1.1" "-" 200 34 3
+
+
+// http://ffbackoffice.hessnatur.com/FF73-Query/Suggest.ff?username=hessnatur&password=d6648543b7ec3c456e38f93d7187a9d8&channel=LIVE-DE-Online-de&format=json&query=matratze
+
+
+// http://ffbackoffice.hessnatur.com/FF73-Query/Suggest.ff?username=hessnatur&password=d6648543b7ec3c456e38f93d7187a9d8&channel=LIVE-DE-Online-de&format=json&query=matratze
+// channel=LIVE-DE-Online-de wird ersetzt
+// &query=matratze ist der Suchbegriff (auf )
+
+// Klick auf Prod
+// WK
+// Gekauft
+
+
+// Login
+
+
+
+
+// Suche mit Filtern
+// http://ffbackoffice.hessnatur.com/FF73-Query/Search.ff?username=hessnatur&password=d6648543b7ec3c456e38f93d7187a9d8&channel=LIVE-DE-Online-de&format=json&query=matratze&filterFFfilterSize%5B%5D=90x200+cm&filterFFfilterSize%5B%5D=090200f&filterFFfilterSize%5B%5D=090200m&filterFFfilterSize%5B%5D=090200w
+
