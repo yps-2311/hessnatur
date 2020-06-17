d7afd0db573843c3032dc6da441ae78cf3e3d9e0
Denis Leno <denis.leno@web-arts.com>
2020-06-17 15:27:47 +0200

    EN Warenkorblayer

---
+++ 2020/10-EN-Warenkorblayer/src/variation-01/script.js
@@ -0,0 +1,14 @@
+// load core and global js
+// @codekit-prepend "../global/global.js";
+
+/**
+ * @function
+ * @author Denis Leno
+ * @namespace V1
+ * @name Variation 01
+ * @description
+ */
+// (function(WATO) {
+//     "use strict";
+
+// })(new window.WATO());
