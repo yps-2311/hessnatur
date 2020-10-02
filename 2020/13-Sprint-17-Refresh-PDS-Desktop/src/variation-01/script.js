930df14a59340b11b182f941d22a1a0e81bb3b58
Michael Klöpping <michael.kloepping@web-arts.com>
2020-10-02 14:10:54 +0200

    13-Sprint-17: fnCallback eingebunden. IF-Prüfung entfernt

--- 2020/13-Sprint-17-Refresh-PDS-Desktop/src/variation-01/script.js
+++ 2020/13-Sprint-17-Refresh-PDS-Desktop/src/variation-01/script.js
@@ -420,18 +420,18 @@
     
                                     newCTLWrapper.insertAdjacentElement('beforeend', CTLProducts[i]);
                                 }
-                                // 02.10.2020: Sicherheitsabfrage
-                                if(CTLProducts.length > 0){
-                                    initGallery('#kk_ctlwrapper', false, CTLProducts.length > 2, 0.41);
-                                }
-                                
+
+                                initGallery('#kk_ctlwrapper', false, CTLProducts.length > 2, 0.41);                               
 
                                 WATO.elem(function(){
                                     // Falls noch nicht geladen
-                                    return typeof window.MagicZoom !== "undefined";
-                                }, function(){
-                                    // init MagicZoom
-                                    window.MagicZoom.start();
+                                    return typeof window.MagicZoom !== "undefined" && typeof window.MagicZoom.start !== "undefined";
+
+                                }, function(fnCallback){
+                                    if(fnCallback){
+                                        // init MagicZoom
+                                        window.MagicZoom.start();
+                                    }
                                 });
                             }
                         }
