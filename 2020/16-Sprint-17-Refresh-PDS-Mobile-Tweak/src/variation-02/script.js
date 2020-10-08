21ea5d3478f46436a5026e7fab03ac5ca827eeb3
Denis Leno <denis.leno@web-arts.com>
2020-10-08 15:38:38 +0200

    fertig für qs

---
+++ 2020/16-Sprint-17-Refresh-PDS-Mobile-Tweak/src/variation-02/script.js
@@ -0,0 +1,54 @@
+// load core and global js
+// @codekit-prepend "../global/global.js";
+
+/**
+ * @function
+ * @author Denis Leno
+ * @namespace V1
+ * @name Variation 01
+ * @description
+ */
+
+
+(function(WATO) {
+    "use strict";
+
+
+    // window.iridion.econda.push(["Sprint17", "V1"]);
+
+
+    WATO.sprint17goals(1);
+    WATO.sprint17(1);
+
+
+    WATO.elem('.pds-cockpit__wrapper ', function(cockpitWrapper){
+        if(cockpitWrapper){
+            cockpitWrapper = cockpitWrapper[0];
+            
+            var mainWrapper = WATO.qs(".js_backstopWrapper"),
+                artNumber = WATO.qs(".pds-cockpit__articleNumber", cockpitWrapper),
+                ratingSummary = WATO.qs(".pds-cockpit__ratingSummaryWrapper");
+
+            if(ratingSummary){
+                artNumber.insertAdjacentElement('afterend', ratingSummary);
+            }
+
+            mainWrapper.insertAdjacentElement('afterbegin', WATO.qs(".pds-cockpit__productName", cockpitWrapper).parentNode.parentNode);
+            mainWrapper.insertAdjacentElement('afterbegin', artNumber.parentNode);
+        }
+    });
+
+    WATO.elem('.pds-cockpit__wrapper', function(cockpitWrapper){
+        if(cockpitWrapper){
+            cockpitWrapper = cockpitWrapper[0];
+            
+            
+            console.log('WATO.qs(".show-for-large", cockpitWrapper): ', WATO.qs(".show-for-large", cockpitWrapper));
+            console.log(' WATO.qs(".align-justify", cockpitWrapper): ',  WATO.qs(".align-justify", cockpitWrapper));
+            WATO.qs(".show-for-large", cockpitWrapper).insertAdjacentElement('afterend', WATO.qs(".align-justify", cockpitWrapper));
+
+        }
+    });
+    
+
+})(new window.WATO());
