ef26b4636439219eb96af6ec354d4b96d7b311e6
Max Vith <max.vith@web-arts.com>
2021-01-14 08:03:29 +0100

    Prototype AB19

--- 2021/01-Sprint-19-PDS_anpassen_SEO/src/variation-01/script.js
+++ 2021/01-Sprint-19-PDS_anpassen_SEO/src/variation-01/script.js
@@ -11,7 +11,7 @@
  */
 (function(WATO) {
     "use strict";
-
+    
     /**
      * CSS Prefix 
      *
@@ -19,49 +19,71 @@
     */
 
 
-    /**
-     * EXAMPLE - POLLING
-     *
-    WATO.elem(".btn-default", function(btnDefault) {
+    WATO.elem('#js-foundation-sticky-left-navigation-data-anchor', function(prodContainer){
 
-        if(btnDefault) {
+        if(prodContainer){
 
+            // create kk root container
+            prodContainer[0].parentNode.insertAdjacentHTML('afterend', 
+                '<div id="kk-seo" class="row">' +
+                    '<div class="columns small-12 large-9 large-offset-3"></div>' +
+                '</div>'
+            );
+
+            // get current SEO text
+            WATO.elem('.footerSmoBoxWrapper .rteContainer', function(seoWrapper){
+
+                if(seoWrapper){
+
+                    var newHTML = '';
+
+                    // we need to wrap the text into seperate parts and add wrapper for product images
+                    seoWrapper[0].innerHTML.split('<h2>').forEach(function(container, index) {
+                        
+                        if(index === 1){
+                            newHTML += '<div class="column small-6 kk-seo-img"><div class="kk-seo-left"></div></div>';
+                        }
+
+                        newHTML += '<div class="column small-6">';
+
+                        if(index !== 0) {
+                            newHTML += '<h2>';
+                        }
+                        
+                        newHTML += container; 
+                        newHTML += '</div>';
+
+                        if(index === 0 || index === 2){
+                            newHTML += '<div class="column small-6 kk-seo-img"><div class="kk-seo-right"></div></div>';
+                        }
+                    });
+                    
+                    console.log(newHTML);
+
+                    WATO.qs('#kk-seo .columns').innerHTML = '<div class="row">' + newHTML + '</div>';
+
+                    // get the first 3 product images
+                    WATO.elem('.productPrgWrapper img.productImage-1', function(prodImages){
+
+                        if(prodImages){
+
+                            var kkSEOImages = WATO.qsa('.kk-seo-img');
+
+                            prodImages.forEach(function(img, index){
+
+                                if(index >= 3)return false;
+
+                                console.log("img", img);
+
+                                if(kkSEOImages[index]){
+
+                                    kkSEOImages[index].children[0].style.backgroundImage = 'url(' + img.getAttribute('src') + ')';
+                                }
+                            });
+                        }
+                    });
+                }
+            });
         }
     });
-
-    // POLLING MIT FUNKTION
-    WATO.elem(function(){return window.numTest === 123;}, function(funcCallback) {
-
-        if(funcCallback){
-
-        }
-    });
-    */
-
-
-    /**
-     * EXAMPLE - MUTATION OBSERVER
-     * 
-     * MUSS ERST IN WATO AKTIVIERT WERDEN
-     *
-    // INIT MUTATION OBSERVER
-    WATO.initObserver(function(error){
-
-        console.log(error);
-    });
-
-    // FIND ELEMENT
-    WATO.observer('.col-md-4', function(cols){
-
-    });
-    */
-
-
-    /**
-     * DOM READY
-     *
-    WATO.ready(function() {
-
-    });
-    */
 })(new window.WATO());
