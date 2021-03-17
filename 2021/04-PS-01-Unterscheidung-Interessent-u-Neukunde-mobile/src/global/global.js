d8757754c776e81fc60eda8931c59983ab94e889
Denis Leno <denis.leno@konversionskraft.de>
2021-03-17 14:23:24 +0100

    test angelegt

---
+++ 2021/04-PS-01-Unterscheidung-Interessent-u-Neukunde-mobile/src/global/global.js
@@ -0,0 +1,299 @@
+/*
+ * !LOAD WATO OBJECT
+ * @codekit-prepend "../vendor/WATO.js";
+ * 
+ * OPTIONAL
+ * @ codekit-append "pds.js"; 
+ */
+
+
+(function(WATO, window){
+    "use strict";
+
+    /*jshint loopfunc: true */
+
+
+    // window.iridion.econda.push(["SprintPS01", "V1"]);
+
+    WATO.prototype.ps01mobile = function(variation){
+        
+        var _self = this,
+            isInteressent = window.iridion.push(['profile', 'getValue', 'customerType']) === "Interessent" && document.location.search.indexOf("show=neukunde") === -1,
+            hessnaturLogo = "https://kk-ffm.s3.eu-central-1.amazonaws.com/hessnatur/2020/ps01/boxlogo.svg", //"https://www.hessnatur.com/medias/sys_master/images/images/hcc/hed/8967611056158/hessnatur-Logo-1c.svg",
+            isPDS = window.document.location.pathname.indexOf("/p/") !== -1,
+            favProducts = [], // Beliebteste Produkte der letzten 3 Monate
+            sehrgefragtProducts = []; // Beliebteste Produkte der letzten 14 Tage
+
+        function iridionProfile(thisName, thisvalue) {
+            if(thisvalue){
+                window.iridion.push(['profile', 'setValue', thisName, JSON.stringify(thisvalue)]);
+            }else{
+                return window.iridion.push(['profile', 'getValue', thisName]);
+            }
+        }
+        
+        if(sessionStorage.getItem("kk_favForSession")){
+            favProducts = JSON.parse(iridionProfile("favProducts"));
+            console.log('1 favProducts: ', favProducts);
+            sehrgefragtProducts = JSON.parse(iridionProfile("sehrgefragtProducts"));
+            console.log('1 sehrgefragtProducts: ', sehrgefragtProducts);
+        }else{
+            _self.xhr_get("https://widgets.crosssell.info/eps/crosssell/recommendations.do?aid=00002762-7fbb585b-0c52-33a0-ad30-b2319526ea2f-1&wid=131&csize=50&start=0&type=cs&widgetdetails=true", function(data) {
+                favProducts = [];
+                var allItems = JSON.parse(data).items;
+                for (var i = 0; i < allItems.length; i++) {
+                    favProducts.push(parseInt(allItems[i].id));
+                }
+                iridionProfile("favProducts", favProducts);
+                sessionStorage.setItem("kk_favForSession", true);
+            });
+    
+            _self.xhr_get("https://widgets.crosssell.info/eps/crosssell/recommendations.do?aid=00002762-7fbb585b-0c52-33a0-ad30-b2319526ea2f-1&wid=132&csize=50&start=0&type=cs&widgetdetails=true", function(data) {
+                sehrgefragtProducts = [];
+                var allItems = JSON.parse(data).items;
+                for (var i = 0; i < allItems.length; i++) {
+                    sehrgefragtProducts.push(parseInt(allItems[i].id));
+                }
+                iridionProfile("sehrgefragtProducts", sehrgefragtProducts);
+                sessionStorage.setItem("kk_favForSession", true);
+            });
+        }
+    
+        function checkForKachelClass(elem) {
+            try {
+                return elem.nextElementSibling ? elem.nextElementSibling.classList.contains('kk_kachel') : false;
+            } catch (error) {
+                return false;
+            }
+        }
+
+        function setBadgeToHead(allProds) {
+            for (var k = 0; k < allProds.length; k++) {
+                var isSaleBadge = _self.qs('.productBadgesWrapper img[src*="sale"], .productBadgesWrapper img[src*="Prozent"]', allProds[k]);
+                if(isSaleBadge){
+                    console.log('isSaleBadge: ', isSaleBadge);
+                    allProds[k].insertAdjacentElement('afterbegin', isSaleBadge.parentNode);
+                }
+            }
+        }
+
+        function getAllProducts() {
+            return _self.qsa(".js-product-grid > .gridviewProductItemWrapper");
+        }
+    
+        function init() {
+    
+            if(isPDS){
+                console.log('isPDS: ', isPDS);
+    
+                // PDS
+                var thisProductID = parseInt(window.location.pathname.split("/p/")[1].substring(0,5));
+    
+                _self.elem('.small-12 > .small-collapse.medium-uncollapse.large-uncollapse', function(contentArea){
+    
+                    if(contentArea){
+    
+                        var productInfo = contentArea[0];
+    
+                        productInfo.insertAdjacentHTML('afterbegin', 
+                            '<div class="kk_quali">'+
+                                '<h4 class="kk_hl">'+
+                                    '<span>N</span>atürliche Qualität für Ihr Wohlbefinden'+
+                                '</h4>'+
+                                '<div class="kk_boxes">'+
+                                    (variation === 1 ? 
+                                        '<div class="kk_natur">'+
+                                            'Naturfasern in höchster<br>Qualität'+
+                                        '</div>'+
+                                        '<div class="kk_stuecke">'+
+                                            'Besonderer Tragekomfort &<br>Verzicht auf Schadstoffe'+
+                                        '</div>'+
+                                        '<div class="kk_tropfen">'+
+                                            'Kleidung, die Mensch & Natur gut tut'+
+                                        '</div>'
+                                        :
+                                        '<img src="https://media.hessnatur.com/kk/2021/ps01-mobile/iconsgroup.svg">'
+                                    )+
+                                '</div>'+
+                            '</div>'
+                        );
+            
+                        var siegel = _self.qs(".certificateWrapper", productInfo);
+    
+                        if(siegel){
+    
+                            siegel.parentNode.classList.add('kk_siegel');
+    
+                            var siegelOrganic = _self.qs('img[src="https://imgs7.hessnatur.com/is/content/HessNatur/Zertifikate/Zertifikat_GOTS.svg"]', siegel);
+    
+                            if(siegelOrganic){
+    
+                                siegelOrganic.setAttribute('src', 'https://kk-ffm.s3.eu-central-1.amazonaws.com/hessnatur/2020/ps01/organic.jpg');
+                            }
+                        }
+                    }
+                });
+    
+                // if(!isInteressent) {
+    
+                // Badges nur für Neukunde
+                _self.elem(function(){
+                    return favProducts.length > 0 && sehrgefragtProducts.length > 0;
+                }, function(element){
+                    if(element){
+    
+                        if(favProducts.indexOf(thisProductID) !== -1){
+                            window.document.documentElement.classList.add('kk_favorit');
+                        }
+    
+                        if(sehrgefragtProducts.indexOf(thisProductID) !== -1){
+                            _self.elem('.pds-cockpit__wrapper', function(cockpitWrapper){
+                                if(cockpitWrapper){
+                                    cockpitWrapper[0].insertAdjacentHTML('afterbegin', 
+                                        '<div class="kk_gefragt"><b>Aktuell sehr gefragt!</b> Dieser Artikel verkauft sich schnell</div>'
+                                    );
+                                }
+                            });
+                        }
+                    }
+                });
+    
+                // }
+    
+                _self.elem('.pds-cockpit__addProductWrapper', function(addToCartBox){
+                    if(addToCartBox){
+                        addToCartBox[0].insertAdjacentHTML('afterend', 
+                            '<div class="kk_cta_uvps">'+
+                                '<ul>'+
+                                    '<li>Kostenlose Rücksendung</li>'+
+                                    '<li>14 Tage Rückgaberecht</li>'+
+                                    '<li>Versand aller Pakete mit DHL</li>'+
+                                '</ul>'+
+                            '</div>'
+                        );
+                    }
+                });
+    
+            } else {
+    
+                // Kategorieseite
+                _self.elem('footer', function(prodWrapper){
+    
+                    if(prodWrapper){
+                        
+                        var allProds = getAllProducts();
+    
+                        console.log('isInteressent: ', isInteressent);
+                        // isInteressent = true;
+                        if(isInteressent && !_self.qs('.kk_hoechstequali')) {
+    
+                            // Interessent
+                            var prod6 = allProds[4] || allProds[allProds.length-2] || allProds[allProds.length-1],
+                                prod9 = allProds[7],
+                                prod15 = allProds[13],
+                                all3boxes = [
+                                    '<div class="gridviewProductItemWrapper column js-product-grid-item kk_kachel kk_hoechstequali">'+
+                                        '<div>'+
+                                            '<div class="kk_content">'+
+                                                '<img src="'+hessnaturLogo+'">'+
+                                                '<h3>Qualität & Bewusstsein </h3>'+
+                                                '<div>Verwendung kontrollierter Naturfasern</div>'+
+                                                '<div>Höchste Qualität bei unseren Produkten</div>'+
+                                                '<div>Mensch und Natur vor<br>Chemikalien schützen</div>'+
+                                            '</div>'+
+                                        '</div>'+
+                                    '</div>',
+                                    '<div class="gridviewProductItemWrapper column js-product-grid-item kk_kachel kk_nachteile">'+
+                                        '<div>'+
+                                            '<div class="kk_content">'+
+                                                '<img src="'+hessnaturLogo+'">'+
+                                                '<h3>Vorteile für Mensch & Natur</h3>'+
+                                                '<div>Konsequente Einhaltung von Naturschutz und Fairness</div>'+
+                                                '<div>Ein Herstellungsprozess mit ökologischer Verantwortung</div>'+
+                                                '<div>Einhaltung des Umweltschutzes<br>(z.B. Abwasserkläranlagen)</div>'+
+                                            '</div>'+
+                                        '</div>'+
+                                    '</div>',
+                                    '<div class="gridviewProductItemWrapper column js-product-grid-item kk_kachel kk_trage">'+
+                                        '<div>'+
+                                            '<div class="kk_content">'+
+                                                '<img src="'+hessnaturLogo+'">'+
+                                                '<h3>Tragekomfort & Verträglichkeit</h3>'+
+                                                '<div>Verwendung von hautsympathischen Materialien</div>'+
+                                                '<div>Verzicht auf belastende Chemikalien</div>'+
+                                                '<div>Wertvolle Naturfasern<br>auf Ihrer Haut</div>'+
+                                            '</div>'+
+                                        '</div>'+
+                                    '</div>'
+                                ];
+    
+                            // Zufällige Reihenfolge
+                            for (var i = all3boxes.length - 1; i > 0; i--) {
+    
+                                var ramdomValue = Math.floor(Math.random() * (i + 1)),
+                                    temp = all3boxes[i];
+    
+                                all3boxes[i] = all3boxes[ramdomValue];
+                                all3boxes[ramdomValue] = temp;
+                            }
+                            
+                            console.log('prod6: ', prod6);
+                            if(prod6 && !checkForKachelClass(prod6)){
+                                prod6.insertAdjacentHTML('afterend', all3boxes[0]);
+                            }
+                            
+                            if(prod9 && !checkForKachelClass(prod9)){
+                                prod9.insertAdjacentHTML('afterend',  all3boxes[1]);
+                            }
+    
+                            if(prod15 && !checkForKachelClass(prod15)){
+                                prod15.insertAdjacentHTML('afterend',  all3boxes[2]);
+                            }
+                        }
+    
+                        // Neukunde
+                        if(!isInteressent) {
+                            _self.elem(function(){
+                                return favProducts.length > 0 && sehrgefragtProducts.length > 0;
+                            }, function(element){
+                                if(element){
+                                    var allProducts = _self.qsa(".dropdown-pane");
+                                    // console.log('allProducts: ', allProducts);
+
+                                    for (var k = 0; k < allProducts.length; k++) {
+                
+                                        var thisProd = allProducts[k],
+                                            thisID = parseInt(thisProd.getAttribute('id').substring(0,5));
+                                        
+                                        if(favProducts.indexOf(thisID) !== -1) {
+                                            thisProd.classList.add('kk_fav');
+                                        }
+
+                                        if(sehrgefragtProducts.indexOf(thisID) !== -1) {
+                                            thisProd.classList.add('kk_sehrgefragt');
+                                        }
+                                    }
+                                }
+                            });
+                        }
+
+                        // Sale Badge wird nach oben rechts verschoben
+                        setBadgeToHead(allProds);
+
+                        setTimeout(function(){
+                            setBadgeToHead(getAllProducts());
+                        }, 1000);
+                    }
+                });
+            }
+        }
+        
+        init();
+    
+        _self.ajax('productListJSON?products', function(){
+    
+            init();
+        });
+    };
+	
+})(window.WATO, window);
---
+++ 2021/04-PS-01-Unterscheidung-Interessent-u-Neukunde-mobile/src/variation-02/script.js
@@ -0,0 +1,16 @@
+// load core and global js
+// @codekit-prepend "../global/global.js";
+
+/**
+ * @function
+ * @author Denis Leno
+ * @namespace V1
+ * @name Variation 01
+ * @description
+ */
+(function(WATO) {
+    "use strict";
+
+    WATO.ps01mobile(2);
+
+})(new window.WATO());
