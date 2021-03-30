3039889472eae23c05c001071d9fd9d95348645e
Denis Leno <denis.leno@konversionskraft.de>
2021-03-30 15:00:37 +0200

    header auf englisch angepasst

--- 100-Prozent-Ausspielungen/HN01.1-Header_TWEAK/variation-01/script.js
+++ 100-Prozent-Ausspielungen/HN01.1-Header_TWEAK/variation-01/script.js
@@ -11,13 +11,18 @@
 
 // window.iridion.econda.push(["HN01", "V1"]);
 
-
-(function(WATO) {
+(function(WATO, window) {
     "use strict";
 
+    var shopInEnglish = window.document.documentElement.getAttribute("lang") === "en";
+
+    console.log('window.location.pathname: ', window.location.pathname);
     if(window.location.pathname.indexOf("/ch/") !== -1){
         window.document.documentElement.classList.add('kk_land_ch');
     }
+    // if(shopInEnglish){
+    //     window.document.documentElement.classList.add('kk_language_en');
+    // }
 
     // hide badges when 0 count
     function checkIcons() {
@@ -38,6 +43,7 @@
     // set navi in fullsize under logo
     WATO.elem('#header', function(header){
         if(header) {
+            console.log(4);
             // add ajax listener for icon badges
             checkIcons();
             WATO.ajax('wishlist/add', checkIcons);
@@ -92,7 +98,7 @@
         if(logo){
             logo[0].setAttribute('src', 
                 'https://media.hessnatur.com/kk/' + 
-                (window.document.documentElement.getAttribute("lang") === "en" ?
+                (shopInEnglish ?
                 'english/logo/hessnatur_en.svg' : 
                 'global/logo/hessnatur_logo.svg'
                 )
@@ -101,7 +107,7 @@
     });
     // }
 
-})(new window.WATO());
+})(new window.WATO(), window);
 
 //!function(e,t){"use strict";void 0===e.WATO&&(e.WATO=function(){}),e.WATO.prototype.elem=function(e,n,o,a,i){var r,l=this||a,d=i||Date.now(),c=false;if(Date.now()-d>1e4)return n(false),false;return"string"==typeof e?c=(r=t.querySelectorAll(e)).length>0:r=c=true===e(),true===c?n(r):setTimeout(l.elem.bind(null,e,n,o,l,d),o||20)},e.WATO.prototype.qs=function(e,n){return(n||t).querySelector(e)},e.WATO.prototype.qsa=function(e,n){return(n||t).querySelectorAll(e)},e.WATO.prototype.ready=function(e){(t.attachEvent?"complete"===t.readyState:"loading"!==t.readyState)?e():t.addEventListener("DOMContentLoaded",e)},e.WATO.prototype.ajax=function(e,t){var n=XMLHttpRequest.prototype.open;XMLHttpRequest.prototype.open=function(o,a,i,r,l){this.addEventListener("loadend",(function(){4===this.readyState&&-1!==a.indexOf(e)&&t()}),false),n.call(this,o,a,i,r,l)}}}(window,document),function(e){"use strict";function t(){for(var t=e.qsa(".meta-navigation .badge--hollow"),n=t.length,o=0;o<n;o++){var a=t[o],i="block";"0"===a.textContent&&(i="none"),a.style.display=i}}e.elem("#header",(function(n){n&&(t(),e.ajax("wishlist/add",t),e.ajax("cart/add",t))}));var n=setInterval((function(){t()}),42);setTimeout((function(){clearInterval(n)}),1e4),e.elem('#mainNavPrgRedirectionForm li[aria-label="Outdoor"]',(function(e){e&&e[0].addEventListener("click",(function(){window.iridion.push(["goal","mainNavi_outdoor","",true])}))})),"/de/outdoor"===location.pathname&&(e.elem('#mainNavPrgRedirectionForm li[aria-label="Damen"] > .h-text-bold',(function(e){e&&e[0].classList.remove("h-text-bold")})),e.elem('#mainNavPrgRedirectionForm li[aria-label="Damen"] .secondLevel .h-text-bold',(function(e){e&&e[0].classList.remove("h-text-bold")})),e.elem('#mainNavPrgRedirectionForm li[aria-label="Damen"] .thirdLevel .h-text-bold',(function(e){e&&e[0].classList.remove("h-text-bold")})),e.elem('#mainNavPrgRedirectionForm [aria-label="Outdoor"] > a',(function(e){e&&e[0].classList.add("h-text-bold")})))}(new window.WATO);
 
