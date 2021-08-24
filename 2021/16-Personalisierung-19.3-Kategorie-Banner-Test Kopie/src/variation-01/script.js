3492c27cc780864ac0e84c6429c687244693e8a4
Max Vith <max.vith@web-arts.com>
2021-08-24 14:36:04 +0200

    upload 19.3

---
+++ 2021/16-Personalisierung-19.3-Kategorie-Banner-Test Kopie/src/variation-01/script.js
@@ -0,0 +1,17 @@
+// load core and global js
+// @ codekit-prepend "../global/global.js";
+// @ codekit-prepend "../../../debugging/enabled.js";
+
+/**
+ * @function
+ * @author Denis Leno
+ * @namespace V1
+ * @name Variation 01
+ * @description
+ */
+
+try {
+    window.iridion.econda.push(["SprintPS19-3", "V1"]);
+} catch (error) {
+    // console.log('Error: ', error);
+}
