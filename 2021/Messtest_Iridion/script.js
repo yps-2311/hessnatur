6709c294861649555b1903ce41d359cc7b6e8056
Nguyet Dang <yen-nguyet.dang@web-arts.com>
2021-11-17 16:06:55 +0100

    Messtest Iridion

---
+++ 2021/Messtest_Iridion/script.js
@@ -0,0 +1,52 @@
+/**
+ * @function
+ * @author Nguyet Dang
+ * @namespace V1
+ * @name Variation 01
+ * @description
+ */
+
+(function () {
+
+    try {
+
+        const getCookie = (name) => {
+            var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+            if (match) return decodeURIComponent(match[2]);
+        };
+        const iridion_user = JSON.parse(getCookie('iridion_user'));
+        const iridion_session = JSON.parse(getCookie('iridion_session'));
+        const emos_jcvid = getCookie('emos_jcvid');
+        const { u: user_id } = iridion_user.find(o => o.u);
+        const experiment_data = iridion_user.find(o => o.t === '1050098069313');
+        const saved_data = JSON.parse(window.localStorage.kk21_messtest || false);
+
+        if (experiment_data) {
+
+            if (saved_data && saved_data.v !== experiment_data.v) {
+                window.iridion.push(['goal', 'messtest_data',
+                    [
+                        Date.now(),
+                        saved_data.user_id,
+                        user_id,
+                        iridion_session,
+                        emos_jcvid,
+                        window.navigator.userAgent,
+                        saved_data.url,
+                        document.URL,
+                        saved_data.v + '>' + experiment_data.v,
+                        saved_data.ts,
+                    ].join('||').replace(/,/g, '_-').replace(/:\/\//g, '_-/-/-').replace(/:/g, '_--')
+                ]);
+            }
+
+            window.localStorage.kk21_messtest = JSON.stringify({ user_id, url: document.URL, ...experiment_data, ts: Date.now() });
+        }
+
+    } catch (error) {
+        window.iridion.push(['goal', 'messtest_error', error.toString()]);
+    }
+    // window.iridion.push(['goal', 'messtest_goal']);
+    // window.iridion.push(['segment', '10070']);
+
+})(window);
