77f7b9594cd2e18746bc58c9a57280b896f460f9
Jonas Hoeppe <jonas.hoeppe@konversionskraft.de>
2022-01-17 15:18:17 +0100

    initial push

---
+++ 2022/01 - AB 29 - Messtest Suchergebnisse/script.js
@@ -0,0 +1,19 @@
+/*
+ * !LOAD WATO OBJECT
+ * @codekit-prepend "../vendor/WATO.js";
+ * 
+ * OPTIONAL
+ * @ codekit-append "pds.js"; 
+ */
+
+
+(function(WATO){
+	"use strict";
+
+	WATO.prototype.messTestsearchResults = function(){
+        
+	};
+	
+    WATO.messTestsearchResults();
+
+})(window.WATO);
