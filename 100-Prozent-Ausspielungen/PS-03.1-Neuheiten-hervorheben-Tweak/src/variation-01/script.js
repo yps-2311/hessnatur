d20deb066db66de8b13cf259919a61dd62e4c52c
Denis Leno <denis.leno@konversionskraft.de>
2022-02-10 17:56:44 +0100

    PS03.1, Consent-Änderung führte zum nicht laden

--- 100-Prozent-Ausspielungen/PS-03.1-Neuheiten-hervorheben-Tweak/src/variation-01/script.js
+++ 100-Prozent-Ausspielungen/PS-03.1-Neuheiten-hervorheben-Tweak/src/variation-01/script.js
@@ -66,131 +66,148 @@
 					'</div>'
 				);
 
-				WATO.elem(function(){
-					return typeof window.econda !== "undefined" && typeof jQuery !== "undefined" && 
-						typeof window.Flickity !== "undefined" && typeof window.usercentrics !== "undefined" && 
-						typeof window.econda.recengine !== "undefined" && typeof window.econda.recengine.Widget !== "undefined"; // typeof jQuery.fn.flickity !== "undefined"
-				} , function(econdaRdy){
-					if(econdaRdy){
-						// Reco-Init aus dem komprimierten Code der Seite von Hessnatur
-						// leicht angepasst "var n = $("#kk_likethisproduct")" und "new window.Flickity(t, ACC.productSlider.getFlickityOptions());"
+				var econdaConsentStatusMarketing = false;
 
-						var e = window.usercentrics.getConsents("SJXtq4iOoZX");
-						if (null !== e && e.consentStatus) {
-							var t = $("#completeTheLookRecommendationsAddToCart");
-							if (t.length > 0) return;
+				// Consent Erlaubnis für Econda
+				var isConsentExists = window.localStorage.getItem("usercentrics");
+				if(isConsentExists){
+					JSON.parse(isConsentExists).consents.forEach(function(item){
+						if(item.categorySlug === "marketing" && item.consentStatus){
+							econdaConsentStatusMarketing = true;
+						}
+					});
+				}else{
+					econdaConsentStatusMarketing = true;
+				}
 
-							var i = {};
-							i.horizontal = function(e, t, i) {
-
-								var sku = WATO.qs('meta[property="product:upc"]').getAttribute('content').substring(0,7);
-								console.log('e.products: ', e.products);
-								e.products = e.products.filter(function(product){
-									return product.sku !== sku;
-								});
-
-								for (var n = e.products, a = [], o = $("#ecRecommendationsAddToCart").length > 0, s = "", r = 0; r < n.length; r++) {
-									var l = n[r]
-										, c = void 0 !== l.oldprice
-										, u = void 0 !== l.basicprice;
-									s += '<div class="' + (o ? "carousel-cell" : "productitem text-center small-8 medium-5 large-3 columns") + '">',
-									s += '<a href="' + (l.deeplink ? l.deeplink : '//www.hessnatur.com/de/p/'+l.sku) + '" class="item__image">',
-									s += '<img src="' + l.iconurl + '" />',
-									s += '<div class="item__desc h-smallOffset-top-outer">',
-									s += '<h4 class="desc-name">' + i.html(l.name) + "</h4>",
-									s += '<div class="desc-price">',
-									s += '<span class="price full ' + (c ? "show" : "hide") + '">' + l.oldprice + "</span>&nbsp;&nbsp;",
-									s += '<span class="price ' + (c ? "show" : "hide") + '" style="margin-left: 3px">' + window.ACC.messages.productPriceFromClean + "</span>",
-									s += '<span class="price special ' + (c ? "show" : "hide") + '">' + l.price + "</span>",
-									s += '<span class="price ' + (c ? "hide" : "show") + '">' + l.price + "</span>",
-									s += u ? '<div class="product-basic-price basicPrice">' + l.basicprice + "</div>" : "",
-									s += "</div>",
-									s += "</div>",
-									s += "</a>",
-									s += "</div>"
-								}
-								a.push(s);
-								var d = a;
-								$(t).html(d);
-								// $(t).flickity(ACC.productSlider.getFlickityOptions())
-								var flickotySlider = new window.Flickity(t, window.ACC.productSlider.getFlickityOptions());
-
-								flickotySlider.on('staticClick', function(event, pointer, cellElement, cellIndex){
-									// KK: PS03: Klick auf das 1. Produkt aus Reco-Element
-									// KK: PS03: Klick auf das 2. Produkt aus Reco-Element
-									// KK: PS03: Klick auf das 3. Produkt aus Reco-Element
-									// KK: PS03: Klick auf das 4. Produkt aus Reco-Element
-									pushGoalAgain('click_product_' + (cellIndex + 1));
-									window.iridion.push(["segment", '32890']);
-								});
-
-								flickotySlider.on('dragStart', function() {
-									pushGoal('click_product_change');
-								});
+                if(econdaConsentStatusMarketing){
+					WATO.elem(function(){
+						return typeof window.econda !== "undefined" && typeof jQuery !== "undefined" && 
+							typeof window.Flickity !== "undefined" && // typeof window.usercentrics !== "undefined" && 
+							typeof window.econda.recengine !== "undefined" && typeof window.econda.recengine.Widget !== "undefined"; // typeof jQuery.fn.flickity !== "undefined"
+					} , function(econdaRdy){
+						if(econdaRdy){
+							// Reco-Init aus dem komprimierten Code der Seite von Hessnatur
+							// leicht angepasst "var n = $("#kk_likethisproduct")" und "new window.Flickity(t, ACC.productSlider.getFlickityOptions());"
+	
+							// var e = window.usercentrics.getConsents("SJXtq4iOoZX");
+							// if (null !== e && e.consentStatus) {
+								var t = $("#completeTheLookRecommendationsAddToCart");
+								if (t.length > 0) return;
+	
+								var i = {};
+								i.horizontal = function(e, t, i) {
+	
+									var sku = WATO.qs('meta[property="product:upc"]').getAttribute('content').substring(0,7);
+									console.log('e.products: ', e.products);
+									e.products = e.products.filter(function(product){
+										return product.sku !== sku;
+									});
+	
+									for (var n = e.products, a = [], o = $("#ecRecommendationsAddToCart").length > 0, s = "", r = 0; r < n.length; r++) {
+										var l = n[r]
+											, c = void 0 !== l.oldprice
+											, u = void 0 !== l.basicprice;
+										s += '<div class="' + (o ? "carousel-cell" : "productitem text-center small-8 medium-5 large-3 columns") + '">',
+										s += '<a href="' + (l.deeplink ? l.deeplink : '//www.hessnatur.com/de/p/'+l.sku) + '" class="item__image">',
+										s += '<img src="' + l.iconurl + '" />',
+										s += '<div class="item__desc h-smallOffset-top-outer">',
+										s += '<h4 class="desc-name">' + i.html(l.name) + "</h4>",
+										s += '<div class="desc-price">',
+										s += '<span class="price full ' + (c ? "show" : "hide") + '">' + l.oldprice + "</span>&nbsp;&nbsp;",
+										s += '<span class="price ' + (c ? "show" : "hide") + '" style="margin-left: 3px">' + window.ACC.messages.productPriceFromClean + "</span>",
+										s += '<span class="price special ' + (c ? "show" : "hide") + '">' + l.price + "</span>",
+										s += '<span class="price ' + (c ? "hide" : "show") + '">' + l.price + "</span>",
+										s += u ? '<div class="product-basic-price basicPrice">' + l.basicprice + "</div>" : "",
+										s += "</div>",
+										s += "</div>",
+										s += "</a>",
+										s += "</div>"
+									}
+									a.push(s);
+									var d = a;
+									$(t).html(d);
+									// $(t).flickity(ACC.productSlider.getFlickityOptions())
+									var flickotySlider = new window.Flickity(t, window.ACC.productSlider.getFlickityOptions());
+	
+									flickotySlider.on('staticClick', function(event, pointer, cellElement, cellIndex){
+										// KK: PS03: Klick auf das 1. Produkt aus Reco-Element
+										// KK: PS03: Klick auf das 2. Produkt aus Reco-Element
+										// KK: PS03: Klick auf das 3. Produkt aus Reco-Element
+										// KK: PS03: Klick auf das 4. Produkt aus Reco-Element
+										pushGoalAgain('click_product_' + (cellIndex + 1));
+										window.iridion.push(["segment", '32890']);
+									});
+	
+									flickotySlider.on('dragStart', function() {
+										pushGoal('click_product_change');
+									});
+									
+									// var scrollGoalSend = false;
+									// flickotySlider.on('scroll', function() {
+									// 	if(!scrollGoalSend){
+									// 		scrollGoalSend = true;
+									// 		// KK: PS03: Klick auf Pfeil auf linken / rechten Seite - PDS - V1
+									// 		pushGoal('click_product_change');
+									// 	}
+									// });
+	
+									// WATO.elem('#kk_likethisproduct > button', (buttons) => {
+									// 	[...buttons].map((button) => {
+									// 		button.addEventListener('click', () => {
+									// 			// KK: PS03: Klick auf Pfeil auf linken / rechten Seite - PDS - V1
+									// 			pushGoal('click_product_change');
+									// 		});
+									// 	});
+									// });
+								};
 								
-								// var scrollGoalSend = false;
-								// flickotySlider.on('scroll', function() {
-								// 	if(!scrollGoalSend){
-								// 		scrollGoalSend = true;
-								// 		// KK: PS03: Klick auf Pfeil auf linken / rechten Seite - PDS - V1
-								// 		pushGoal('click_product_change');
-								// 	}
-								// });
-
-								// WATO.elem('#kk_likethisproduct > button', (buttons) => {
-								// 	[...buttons].map((button) => {
-								// 		button.addEventListener('click', () => {
-								// 			// KK: PS03: Klick auf Pfeil auf linken / rechten Seite - PDS - V1
-								// 			pushGoal('click_product_change');
-								// 		});
-								// 	});
-								// });
-							};
-							
-							var n = $("#kk_likethisproduct"), 
-								a = n.data("accountid"), 
-								o = n.data("wid"), 
-								s = n.data("product"), 
-								r = n.data("count"),
-								l = new window.econda.recengine.Widget({
-									element: n,
-									renderer: {
-										type: "function",
-										rendererFn: i.horizontal
-									},
-									accountId: a,
-									id: o,
-									context: {
-										products: [{
-											id: s
-										}]
-									},
-									chunkSize: r,
-									empty: function(e) {
-										this._onSuccessfulResponse(e)
+								var n = $("#kk_likethisproduct"), 
+									a = n.data("accountid"), 
+									o = n.data("wid"), 
+									s = n.data("product"), 
+									r = n.data("count"),
+									l = new window.econda.recengine.Widget({
+										element: n,
+										renderer: {
+											type: "function",
+											rendererFn: i.horizontal
+										},
+										accountId: a,
+										id: o,
+										context: {
+											products: [{
+												id: s
+											}]
+										},
+										chunkSize: r,
+										empty: function(e) {
+											this._onSuccessfulResponse(e)
+										}
+									});
+	
+								l.render();
+	
+								WATO.elem('#kk_likethisproduct .flickity-prev-next-button.previous', function(prevBtn){
+									if(prevBtn){
+										prevBtn[0].addEventListener('click', function(){
+											pushGoal('click_product_change');
+										});
 									}
 								});
-
-							l.render();
-
-							WATO.elem('#kk_likethisproduct .flickity-prev-next-button.previous', function(prevBtn){
-								if(prevBtn){
-									prevBtn[0].addEventListener('click', function(){
-										pushGoal('click_product_change');
-									});
-								}
-							});
-							WATO.elem('#kk_likethisproduct .flickity-prev-next-button.next', function(nextBtn){
-								if(nextBtn){
-									nextBtn[0].addEventListener('click', function(){
-										pushGoal('click_product_change');
-									});
-								}
-							});
-
+								WATO.elem('#kk_likethisproduct .flickity-prev-next-button.next', function(nextBtn){
+									if(nextBtn){
+										nextBtn[0].addEventListener('click', function(){
+											pushGoal('click_product_change');
+										});
+									}
+								});
+	
+							// }
 						}
-					}
-				});
+					});
+
+				}
 			}
 		});
 	}
