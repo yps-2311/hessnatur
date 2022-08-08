3f78000fba1c3d3f62cd8da3e6b8916c639309b0
Christian Knoth <christian.knoth@konversionskraft.de>
2022-08-08 15:10:25 +0200

    splitted test

---
+++ 2022/05-Sprint_AB_30-Versandkostenfreigrenzen/variation-02/script.js
@@ -0,0 +1,18 @@
+// load core and global js
+// @codekit-prepend "../global/global.js";
+
+/**
+ * @function
+ * @author Christian Knoth
+ * @namespace V1
+ * @name Variation 01
+ * @description
+ */
+
+(function(WATO) {
+    "use strict";
+
+    WATO.S0522("v2");
+
+})(new window.WATO());
+//})(window.WATO); <- script standalone
---
+++ 2022/05-Sprint_AB_30-Versandkostenfreigrenzen/variation-03/script.js
@@ -0,0 +1,19 @@
+// load core and global js
+// @codekit-prepend "../global/global.js";
+
+/**
+ * @function
+ * @author Christian Knoth
+ * @namespace V1
+ * @name Variation 01
+ * @description
+ */
+
+(function(WATO) {
+    "use strict";
+    
+    
+    WATO.S0522("v3");
+
+})(new window.WATO());
+//})(window.WATO); <- script standalone
