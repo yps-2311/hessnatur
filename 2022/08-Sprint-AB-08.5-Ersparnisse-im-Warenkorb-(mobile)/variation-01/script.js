d0a9c39f0b43155812d1b42a6bc4b42ac9ccf790
Jonas Hoeppe <jonas.hoeppe@konversionskraft.de>
2022-12-20 08:16:47 +0000

    Sprint ab 08.5 ersparnisse im warenkorb

---
+++ 2022/08-Sprint-AB-08.5-Ersparnisse-im-Warenkorb-(mobile)/variation-01/script.js
@@ -0,0 +1,21 @@
+// load core and global js
+// @ codekit-prepend "../global/global.js";
+
+/**
+ * @function
+ * @author Jonas Hoeppe
+ * @namespace V1
+ * @name Variation 01
+ * @description
+ */
+ (function(WATO) {
+    "use strict";
+
+     // Econda
+     window.iridion.econda.push(["AB8.5", "V1"]);
+
+    WATO.s8_5(1);
+
+    WATO.s8_5_GOALS(1);
+
+})(new window.WATO());
