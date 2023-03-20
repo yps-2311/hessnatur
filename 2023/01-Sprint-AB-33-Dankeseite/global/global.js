e7a56571104ba6d9ecf5fdf86f75fd19ec301e65
= <=>
2023-03-20 10:31:55 +0100

    initial push

---
+++ 2023/01-Sprint-AB-33-Dankeseite/global/global.js
@@ -0,0 +1,18 @@
+/*
+ * !LOAD WATO OBJECT
+ * @codekit-prepend "../vendor/WATO.js";
+ * @prepros-prepend "../vendor/WATO.js";
+ * 
+ * OPTIONAL
+ * @ codekit-append "pds.js"; 
+ */
+
+/*
+(function(WATO){
+	"use strict";
+
+	WATO.prototype.MUSTERSPRINT = function(){
+	};
+	
+})(window.WATO);
+*/
---
+++ 2023/01-Sprint-AB-33-Dankeseite/variation-01/script.js
@@ -0,0 +1,84 @@
+// load core and global js
+// @codekit-prepend "../global/global.js";
+// @prepros-prepend "../global/global.js";
+
+/**
+ * @function
+ * @author Max Mustermann
+ * @namespace V1
+ * @name Variation 01
+ * @description
+ */
+(function(WATO) {
+    "use strict";
+
+    /**
+     * CSS Prefix 
+     *
+    document.documentElement.classList.add('specific-experiment-class');
+    */
+
+    /**
+     * EXAMPLE - POLLING
+     *
+    WATO.elem(".btn-default", function(btnDefault) {
+
+        if(btnDefault) {
+
+        }
+    });
+
+    // POLLING MIT FUNKTION
+    WATO.elem(function(){return window.numTest === 123;}, function(funcCallback) {
+
+        if(funcCallback){
+
+        }
+    });
+    */
+
+
+    /**
+     * EXAMPLE - MUTATION OBSERVER
+     * 
+     * MUSS ERST IN WATO AKTIVIERT WERDEN
+     *
+    // INIT MUTATION OBSERVER
+    WATO.initObserver(function(error){
+
+        console.log(error);
+    });
+
+    // FIND ELEMENT
+    WATO.observer('.col-md-4', function(cols){
+
+    });
+    */
+
+
+    /**
+     * DOM READY
+     *
+    WATO.ready(function() {
+
+    });
+    */
+
+    /**
+     * EXAMPLE - Event
+     *
+    // with polling
+    WATO.ev('#main-cta', 'click', function(){
+        console.log("click it");
+    });
+
+    // without polling
+    WATO.elem('#main-cta', function(elem){
+
+        WATO.ev(elem[0], 'click', function(){
+            console.log("click it");
+        });
+    });
+    */
+   
+})(new window.WATO());
