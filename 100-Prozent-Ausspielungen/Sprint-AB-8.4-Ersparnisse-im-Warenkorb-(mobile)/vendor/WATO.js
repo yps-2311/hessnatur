133912f2ea6efa109fb4aa8d571e6e613bddfdaf
Denis Leno <denis.leno@konversionskraft.de>
2023-11-23 15:10:15 +0100

    sprint 8.4 mobile - xhr_get funktion wurde ausgel.

--- 100-Prozent-Ausspielungen/Sprint-AB-8.4-Ersparnisse-im-Warenkorb-(mobile)/vendor/WATO.js
+++ 100-Prozent-Ausspielungen/Sprint-AB-8.4-Ersparnisse-im-Warenkorb-(mobile)/vendor/WATO.js
@@ -510,32 +510,32 @@
      * @author Lukas Dziambor
      */
     
-	window.WATO.prototype.xhr_get = function(url, scopedData, callback) {
-        var request = new XMLHttpRequest();
-        request.open('GET', url, true);
+	// window.WATO.prototype.xhr_get = function(url, scopedData, callback) {
+    //     var request = new XMLHttpRequest();
+    //     request.open('GET', url, true);
         
-        request.onload = function() {
-          if (this.status >= 200 && this.status < 400) {
-            try {
-                var data = JSON.parse(this.response);
-                callback(data, scopedData);
-            }
-            catch(e) {
-                callback(data);    
-            }
-          } else {
-                // We reached our target server, but it returned an error
-                callback(false);
-          }
-        };
+    //     request.onload = function() {
+    //       if (this.status >= 200 && this.status < 400) {
+    //         try {
+    //             var data = JSON.parse(this.response);
+    //             callback(data, scopedData);
+    //         }
+    //         catch(e) {
+    //             callback(data);    
+    //         }
+    //       } else {
+    //             // We reached our target server, but it returned an error
+    //             callback(false);
+    //       }
+    //     };
         
-        request.onerror = function() {
-            // There was a connection error of some sort
-            callback(false);
-        };
-        request.withCredentials = true;
-        request.send();
-    };
+    //     request.onerror = function() {
+    //         // There was a connection error of some sort
+    //         callback(false);
+    //     };
+    //     request.withCredentials = true;
+    //     request.send();
+    // };
     
 
     /**
