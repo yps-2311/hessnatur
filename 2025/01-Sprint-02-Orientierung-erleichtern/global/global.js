d4dc989481463bfb5f2a4a590e5843debe4c89aa
Andreas Hartung <andreas.hartung@konversionskraft.de>
2025-11-20 12:03:54 +0100

    final

--- 2025/01-Sprint-02-Orientierung-erleichtern/global/global.js
+++ 2025/01-Sprint-02-Orientierung-erleichtern/global/global.js
@@ -17,32 +17,5 @@
 (function(KEK) {
     "use strict";
 
-    // KEK.prototype.KamPo = Kameleoon.API.Core.runWhenElementPresent;
-    
-    // const pushGoal = (goalID) => {
-    //     if (window.Kameleoon && window.Kameleoon.API && window.Kameleoon?.API?.Goals) {
-    //         window.Kameleoon.API.Goals.processConversion(goalID);
-    //     }
-    // }
-    
-
-    KEK.prototype.S02Excluded = function(){
-
-        const KEK = this;
-
-        const cookieVal = "kk_S02_exlude";
-
-        if(KEK.cookieGet(cookieVal) === "true"){
-            return true;
-        }
-
-        KEK.exclude(1100, () => {
-            
-            KEK.cookieSet(cookieVal, "true", "www.hessnatur.com");
-            KEK.reload();
-        });
-
-        return false;
-    };
 	
 })(window.KEK);
