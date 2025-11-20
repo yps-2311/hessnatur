2ee9d9833bbcf4e80164acf6614e862fec100b44
Andreas Hartung <andreas.hartung@konversionskraft.de>
2025-11-20 14:19:00 +0100

    final sp07

---
+++ 2025/02-Sprint-07-Groeßenauswahl-Alertlayer-vs-ausgeklappte-Groeßen/variation-01/script.js
@@ -0,0 +1,66 @@
+// @codekit-prepend "../global/global.js";
+// @prepros-prepend "../global/global.js";
+/**
+ * @function
+ * @author Andreas Hartung
+ * @namespace Variante1
+ * @name Variation 01
+ * @description
+ */
+(function (KEK) {
+  "use strict";
+  //console.log("hessantur SP07 Variante1 Update: 5");
+
+  KEK.elem('[data-testid="add-to-cart"]', (wrapper) => {
+
+    if (!wrapper || !wrapper[0]) return;
+    wrapper = wrapper[0];
+
+    if (wrapper.classList.contains("kk_sp07_a2c")) {
+      return;
+    }
+
+    wrapper.classList.add("kk_sp07_a2c");
+
+    wrapper.addEventListener("click", (e) => {
+
+      // Den tatsächlichen Button finden (egal wie verschachtelt)
+      const realBtn = e.target.closest('[data-testid="add-to-cart-button"]');
+
+      if (!realBtn) {
+        return;
+      }
+
+      // Jetzt prüfen ob der Button disabled ist
+      const isDisabled =
+        realBtn.matches('[class*="Button_button--disabled"]') ||
+        realBtn.getAttribute("aria-disabled") === "true";
+
+      if (!isDisabled) {
+        return;
+      }
+
+
+      // Jetzt React-Select öffnen
+      const input = KEK.qs("input#ProductSizeSelector");
+      if (!input) {
+        return;
+      }
+
+      input.focus();
+      input.dispatchEvent(
+        new KeyboardEvent("keydown", {
+          key: "ArrowDown",
+          code: "ArrowDown",
+          keyCode: 40,
+          bubbles: true,
+          cancelable: true
+        })
+      );
+
+
+    });
+  });
+
+})(new window.KEK());
+
