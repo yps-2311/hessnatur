b19b90d8b4936669ef3231316758067efab072c0
Manuel Brückmann <manuel.brueckmann@konversionskraft.de>
2026-01-29 11:47:17 +0100

    2026/MBr/Sprint-5-USP-Benefit-Box
    feat: USP-Benefit-Box Carousel (Mobile View) umgesetzt
    
    - Carousel mit 3 USP-Slides (Qualität, Nachhaltigkeit, Service)
    - Touch/Swipe Navigation mit Infinite Loop
    - Drawer-Komponente für Detail-Ansicht
    - Page-Type Detection via data-testid (homepage, category, pdp)
    - MutationObserver für SPA-Navigation
    - SessionStorage-basierte Slide-Rotation zwischen Seitenwechseln
    - Kontextabhängige Platzierung je Seitentyp
    
    fix: PDP Fallback für Produkte ohne story-telling
    - Unterstützung für data-totalretracedstory als alternatives Target

---
+++ 2026/02-Sprint-05-USP-Benefit-Box/src/variation-00/script.js
@@ -0,0 +1,17 @@
+// load core and global js
+// @codekit-prepend "../global/global.js";
+// @prepros-prepend "../global/global.js";
+
+/**
+ * @function
+ * @author Max Mustermann
+ * @namespace V0
+ * @name Variation 00
+ * @description
+ */
+(function(KEK) {
+    "use strict";
+
+    //KEK.MUSTERSPRINT(0);
+
+})(new window.KEK());
